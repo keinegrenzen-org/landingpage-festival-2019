5ffc07ef1c2e936b64412e28ecb58f1d30ac4851
BarthyB <barthy.bonhomme@gmail.com>
2018-12-30 12:50:55 +0100

    zahlen

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -18,4 +18,93 @@ class DefaultController extends Controller
             'index.html.twig'
         );
     }
+
+    /**
+     * @Route("/zahlen", name="numbers")
+     * @throws \Exception
+     */
+    public function numbers()
+    {
+        $auth = 'Token '.$_SERVER['PRETIX_TOKEN'];
+        $base = 'https://tickets.keinegrenzen.org';
+        $path = '/api/v1/organizers/event/events/festival/orders/';
+
+        $headers = array(
+            'Accept: application/json',
+            "Authorization: $auth",
+        );
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $next = null;
+        $orders = [];
+
+        do {
+
+            $query = http_build_query(
+                [
+                    'page' => $next,
+                    'status' => 'p',
+                ]
+            );
+
+            curl_setopt($curl, CURLOPT_URL, $base.$path.'?'.$query);
+
+            $response = curl_exec($curl);
+            $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+            if ($code == 200) {
+                $response = json_decode($response, true);
+                $next = $response['next'];
+                $orders = array_merge($orders, $response['results']);
+            } else {
+                $response = json_decode($response, true);
+                curl_close($curl);
+                throw new \Exception($response['detail'], $code);
+            }
+
+        } while ($next !== null);
+
+        curl_close($curl);
+
+        $gross = 0;
+        $net = 0;
+
+        foreach ($orders as $order) {
+            $payment = $order['payments'][0];
+            $total = floatval($order['total']);
+            $gross += $total;
+
+            switch ($payment['provider']) {
+                case 'paypal':
+                    $net += $total - 0.35 - ($total * 0.0249);
+                    break;
+                case 'stripe':
+                    $net += $total - 0.25 - ($total * 0.014);
+                    break;
+            }
+        }
+
+        $net = round($net, 2);
+
+        $booking = 856 + 140.34 + 156.29 + 600 + 547 + 140.2;
+        $promo = 30;
+        $print = 62.21;
+
+        $sum = -$booking - $print - $promo + $net;
+
+        return $this->render(
+            'numbers.html.twig',
+            [
+                'booking' => $booking,
+                'promo' => $promo,
+                'print' => $print,
+                'gross' => $gross,
+                'net' => $net,
+                'sum' => $sum,
+            ]
+        );
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var Encore = require('@symfony/webpack-encore')
 
 Encore
+  .enableSingleRuntimeChunk()
   .setOutputPath('public/build/')
   .setPublicPath('/build')
   .cleanupOutputBeforeBuild()
@@ -10,6 +11,8 @@ Encore
   .addEntry('js/theme', './assets/js/theme.js')
   .addEntry('js/SoundCloud', './assets/js/SoundCloud.js')
   .addStyleEntry('css/theme', './assets/css/theme.scss')
+  .addStyleEntry('css/numbers', './assets/css/numbers.scss')
+  .addStyleEntry('css/index', './assets/css/index.scss')
 
   .enableSassLoader()
   .enablePostCssLoader()
