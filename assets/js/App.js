34293acdf62c3ea706c33e7df40a298e456b029d
barthy <b.bonhomme@vucx.de>
2023-03-04 12:41:00 +0100

    cookies fix

--- assets/js/App.js
+++ assets/js/App.js
@@ -31,7 +31,7 @@ export class App {
   }
 
   initYouTube () {
-    if (!this.acceptedEmbeddings.includes('YouTube Cookies (ytimg)')) {
+    if (!this.acceptedEmbeddings.includes('Youtube Cookies (ytimg)')) {
       return
     }
 
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,5 +3,5 @@ import { App } from './App'
 import '../css/theme.scss'
 
 document.addEventListener('DOMContentLoaded', function () {
-  new App()
+  window.app = new App()
 }, false)
