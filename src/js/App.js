5d25f1b1c60a53c96eeacbd368c8620033e9dcce
Barthy <post@barthy.koeln>
2023-10-30 17:38:05 +0000

    Native lazyload and native smooth scroll

--- src/js/App.js
+++ src/js/App.js
@@ -1,24 +1,5 @@
 import GLightbox from 'glightbox'
 
-export function initSmoothScroll() {
-    const triggers = document.querySelectorAll('.scroll-to')
-    for (let trigger of triggers) {
-        trigger.addEventListener('click', e => {
-            e.preventDefault()
-
-            const clickTarget = e.currentTarget
-            const scrollTarget = clickTarget.getAttribute('href')
-
-            const targetElement = document.getElementById(scrollTarget.replace('#', ''))
-            const rect = targetElement.getBoundingClientRect()
-            const distance = rect.top
-            window.scrollBy({top: distance, behavior: 'smooth'})
-
-            history.replaceState({}, '', scrollTarget)
-        }, false)
-    }
-}
-
 export function initSoundCloudFrame() {
     const title = document.querySelector('.soundcloud .title')
     const frame = title.parentNode
@@ -28,7 +9,7 @@ export function initSoundCloudFrame() {
     })
 }
 
-export function initLazyLoad() {
+export function initLightbox() {
     const masonry = document.querySelector('.masonry')
     const images = masonry.querySelectorAll('.masonry-image')
     const lightBoxElements = []
@@ -45,19 +26,4 @@ export function initLazyLoad() {
     }
 
     const lightBox = new GLightbox({elements: lightBoxElements})
-
-    const observer = new IntersectionObserver(function (entries, observer) {
-        for (const entry of entries) {
-            if (entry.intersectionRatio > 0) {
-                observer.unobserve(entry.target)
-
-                const image = entry.target
-                image.src = image.dataset.src
-            }
-        }
-    })
-
-    for (const image of images) {
-        observer.observe(image)
-    }
 }
